9858b7b542de3a60bc78bb8c29a4b77ccb172834
Jowan <patryk.jowanowski@neoteric.eu>
2023-12-29 15:48:29 +0100

    new changes

--- e2e/demoqa_elements.spec.ts
+++ e2e/demoqa_elements.spec.ts
@@ -1,12 +1,31 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://demoqa.com/elements');
+
+
+  const checkBox = page.getByText("Check Box");
+  const radioButton = page.getByText("Radio Button");
+  const webTables = page.getByText("Web Tables");
+  const buttons = page.getByText("Buttons");
+  const links = page.getByText("Links");
+  const brokenLinks = page.getByText("Broken Links");
+  const uploadNDownload = page.getByText("Upload and Download");
+  const dynamicProperties = page.getByText("Dynamic Properties");
+
+});
+
 test.describe('Demo QA - elements page', () => {
-  test('has title', async ({ page }) => {
-    await page.goto('https://demoqa.com/elements');
+  test('has title "DEMOQA" and proper body message', async ({ page }) => {
     await expect(page).toHaveTitle("DEMOQA");
+    await expect(page.getByText('Please select an item from left to start practice.')).toBeVisible();
   });
 
   test('opens elements Text Box', async ({ page }) => {
+    const textBox = page.getByText("Text Box");
+
     await expect(page.getByText('Elements').nth(1)).toBeVisible();
+    await expect(textBox).toHaveText("Text Box");
+    await textBox.click();
   });
 });
