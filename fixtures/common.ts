0d54c2c907b54a8800c42b22cdb9e37830324663
Jowan <patryk.jowanowski@neoteric.eu>
2024-01-08 12:53:08 +0100

    commons extraction cd

--- fixtures/common.ts
+++ fixtures/common.ts
@@ -11,10 +11,28 @@ export const common = {
 		sections: {
 			elements: "Elements",
 			textBox: "Text Box",
-			checkBox: "Check Box"
+			checkBox: "Check Box",
+			radioButton: "Radio Button",
+			webTables: "Web Tables",
+			buttons: "Buttons",
+			links: "Links",
+			brokenLinksImages: "Broken Links - Images",
+			uploadNDownload: "Upload and Download",
+			dynamicProperties: "Dynamic Properties"
 		},
+		responses: {
+			201: "Link has responded with staus 201 and status text Created",
+			204: "Link has responded with staus 204 and status text No Content",
+			301: "Link has responded with staus 301 and status text Moved Permanently",
+			400: "Link has responded with staus 400 and status text Bad Request",
+			401: "Link has responded with staus 401 and status text Unauthorized",
+			403: "Link has responded with staus 403 and status text Forbidden",
+			404: "Link has responded with staus 404 and status text Not Found"
+		}
 	},
 	url: {
-		elements: "https://demoqa.com/elements"
+		elements: "https://demoqa.com/elements",
+		valid: "http://demoqa.com",
+		invalid: "http://the-internet.herokuapp.com/status_codes/500",
 	}
 };
